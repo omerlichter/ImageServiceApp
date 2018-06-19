5a77c806355ffec0bdf5f2e9395d70a55fb8131c
Dvir Plaksin <dvirpla@gmail.com>
2018-06-19 12:24:40 +0300

    changes

--- app/src/main/java/com/example/omer/imageservice/ImageServiceService.java
+++ app/src/main/java/com/example/omer/imageservice/ImageServiceService.java
@@ -1,5 +1,7 @@
 package com.example.omer.imageservice;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -10,9 +12,11 @@ import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
@@ -22,6 +26,7 @@ import android.widget.Toast;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
@@ -51,6 +56,7 @@ public class ImageServiceService extends Service {
     public int onStartCommand(Intent intent, int flag, int startID) {
         Toast.makeText(this, "Service starting...", Toast.LENGTH_SHORT).show();
         this.reciver = new BroadcastReceiver() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onReceive(Context context, Intent intent) {
                 WifiManager wifiManager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
@@ -59,7 +65,7 @@ public class ImageServiceService extends Service {
                     if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
                         //get the different network states
                         if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
-                            transferImages();
+                            transferImages(context);
                         }
                     }
                 }
@@ -77,7 +83,8 @@ public class ImageServiceService extends Service {
         Toast.makeText(this, "Service ending...", Toast.LENGTH_SHORT).show();
     }
 
-    private void transferImages() {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void transferImages(Context context) {
         Toast.makeText(this, "sending images", Toast.LENGTH_SHORT).show();
         File Camera = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "Camera");
         if (Camera == null) {
@@ -86,10 +93,16 @@ public class ImageServiceService extends Service {
         final File[] pics = Camera.listFiles();
 
         // progress bar
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
-        builder.setContentTitle("Picture Transfer").setContentText("Transfer in progress").setPriority(NotificationCompat.PRIORITY_LOW);
+        final int NI = 1;
+        NotificationChannel NC = new NotificationChannel("default", "default", NotificationManager.IMPORTANCE_DEFAULT);
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default")
+        .setSmallIcon(R.drawable.ic_launcher_foreground).setContentTitle("BackUp The Pics").setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        final NotificationManager notificationManager = (NotificationManager) getSystemService(context.NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(NC);
+        builder.setProgress(100, 0,false);
 
+        builder.setContentTitle("Picture Transfer").setContentText("Transfer in progress").setPriority(NotificationCompat.PRIORITY_LOW);
+        notificationManager.notify(10, builder.build());
 
         int count = 0;
         if (pics != null) {
@@ -98,12 +111,12 @@ public class ImageServiceService extends Service {
                 public void run() {
                     try {
                         InetAddress serverAddr = InetAddress.getByName("10.0.2.2");
-                        Socket socket = new Socket(serverAddr, 8000);
                         try {
-                            OutputStream outputStream = socket.getOutputStream();
-                            InputStream inputStream = socket.getInputStream();
-
+                            int bar = 0;
                             for (File pic : pics) {
+                                Socket socket = new Socket(serverAddr, 8000);
+                                OutputStream outputStream = socket.getOutputStream();
+                                InputStream inputStream = socket.getInputStream();
                                 Log.e("TCP", "YES!");
                                 FileInputStream fis = new FileInputStream(pic);
                                 Bitmap bm = BitmapFactory.decodeStream(fis);
@@ -114,11 +127,16 @@ public class ImageServiceService extends Service {
                                     outputStream.write(imgbyte);
                                 }
                                 outputStream.flush();
+                                bar = bar + 100 / pics.length;
+                                builder.setProgress(100, bar, false);
+                                notificationManager.notify(10, builder.build());
+                                socket.close();
                             }
+                            builder.setProgress(0,0, false);
+                            builder.setContentText("Finish Transfer.");
+                            notificationManager.notify(10, builder.build());
                         } catch (Exception e) {
                             Log.e("TCP", "S: Error", e);
-                        } finally {
-                            socket.close();
                         }
                     } catch (Exception e) {
                         Log.e("TCP", "C: Error", e);
@@ -133,6 +151,22 @@ public class ImageServiceService extends Service {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG, 70, stream);
         return stream.toByteArray();
+    }
+
+    public static byte[] extractBytes(File file) throws IOException {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        FileInputStream fis = new FileInputStream(file);
+        try {
+            int i;
+            while ((i = fis.read(buffer)) != -1) {
+                stream.write(buffer, 0, i);
+            }
+
+        } catch (IOException ex) {
+        }
+        return stream.toByteArray();
+
     }
 
 }
